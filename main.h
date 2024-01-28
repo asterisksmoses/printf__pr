8081d2cb4ea22606e66faaa38afcb41034bfa0d0
ChrisMwangi1 <wanjikuchristine201@gmail.com>
2024-01-28 00:27:17 -0800

    First commit of the repo, containing a readme, header file, and putchar

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+int _printf(const char *format, ...);
+int tm_putchar(char c);
+
+#endif
---
+++ tm_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * tm_putchar - Print a character to stdout
+ * @c: character to be printed
+ *
+ * Return: 1 (Success)
+ */
+
+int tm_putchar(char c)
+{
+	return (write(1, &c, 1);
+}
